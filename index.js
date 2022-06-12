6f0a061322b12998a83facafda464b2bc0d04a3a
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-12 17:02:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+const names=["Charlie", "Samip", "Ali"];
+function writeCards(names, event) {
+    let messages=[]
+    for (let i = 0; i < names.length; i++) {
+
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+
+        const message = `Thank you, ${names[i]} for the wonderful ${event} gift!`
+        messages.push(message);
+        console.log(i);
+        
+    }
+    return messages;
+
+}
+writeCards(Guadalupe, surprise)
+writeCards(Ollie, surprise)
+writeCards(Aki, surprise)
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+function countDown(){
+
+    let num = 10;
+    
+    while (num>=0){
+    
+    console.log(num)
+    }
+}       
