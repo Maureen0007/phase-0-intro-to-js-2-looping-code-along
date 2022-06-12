1b2318bf3244a67d25f08a4d29d630bf7d5f3c8b
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-12 16:56:17 +0300

    commit tree f12419a2e24e5289a8fb74c34fd826559bb7763e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names=["Charlie", "Samip", "Ali"];
+function writeCards(names, event) {
+    let messages=[]
+    for (let i = 0; i < names.length; i++) {
+
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+
+        const message = `Thank you, ${names[i]} for the wonderful${event} gift!`
+        messages.push(message);
+        console.log(i);
+        
+    }
+    return messages;
+    
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
